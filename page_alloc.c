875c30e4ba542d37c976627cf7e72523b9b73737
01123581321345589 <44416392+01123581321345589@users.noreply.github.com>
2018-12-01 13:50:26 -0500

    Add files via upload

--- page_alloc.c
+++ page_alloc.c
@@ -7,6 +7,7 @@
 #include <linux/moduleparam.h>
 #include <linux/gfp.h>
 #include <linux/log2.h>
+#include <linux/time.h>
 
 unsigned int objnum = 2000;
 module_param(objnum, uint, 0);
@@ -16,18 +17,18 @@ struct type {
 };
 
 int threadfn(void *data){
-    unsigned long tick0, tick1;
+    struct timeval t0, t1;
     struct page * p;
     int order;
     printk(KERN_INFO "number of objects: %u\n", *(unsigned int *)data);
     printk(KERN_INFO "size of the structure: %zd\n", sizeof(struct type));
     order = order_base_2(objnum)-order_base_2(div_u64(PAGE_SIZE, sizeof(struct type)));
     printk(KERN_INFO "allocating 2^%d pages\n", order);
-    tick0 = jiffies;
+    do_gettimeofday(&t0);
     p = alloc_pages(GFP_KERNEL, order);
     __free_pages(p, order);
-    tick1 = jiffies;
-    printk(KERN_INFO "takes %ld microseconds\n", (tick1-tick0)*1000000/HZ);
+    do_gettimeofday(&t1);
+    printk(KERN_INFO "takes %ld microseconds\n", t1.tv_usec-t0.tv_usec);
     return 0;
 }
 
